3430c519c3fd4bcdb871d8c05b167ba2a0d86e11
Neo Swann <nswann11@gmail.com>
2024-10-16 09:59:41 -0400

    mobile menu update

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -7,12 +7,10 @@ export default async function Layout({ children }) {
       {/* Sidebar | Left */}
       <Sidebar />
       {/* Main content | Right */}
-      <div className="h-full w-full flex-1">
+      <div className="flex h-full w-full flex-1 flex-col">
         {/* Top Bar */}
         <Topbar />
-        <main
-          className={`h-[calc(100%-72px)] overflow-y-auto p-4 md:p-5 lg:p-8`}
-        >
+        <main className={`flex-1 overflow-y-auto p-4 md:p-5 lg:p-8`}>
           {children}
         </main>
       </div>
--- components/MobileMenu/MobileMenu.jsx
+++ components/MobileMenu/MobileMenu.jsx
@@ -1,62 +1,49 @@
-import NavLink from '../NavLink'
-import { LayoutDashboard, MonitorSmartphone, Users, ClipboardList, HelpCircle, LogOut, X } from 'lucide-react'
+import NavLink from "../NavLink";
+import {
+  LayoutDashboard,
+  MonitorSmartphone,
+  Users,
+  ClipboardList,
+  HelpCircle,
+  LogOut,
+  X,
+} from "lucide-react";
 import "./MobileMenu.scss";
-import Logo from '../Logo';
-
+import Logo from "../Logo";
 
 const MobileMenu = ({ className, closeMenu }) => {
+  return (
+    <div className={`mobile-menu ${className}`}>
+      <div className="cont">
+        <div className="top-wrapper">
+          <Logo />
 
-    return (
-        <div className={`mobile-menu ${className}`}>
-            <div className="cont">
-                <div className="top-wrapper">
-                    <Logo />
-
-                    <X size={32} onClick={closeMenu} className='cursor-pointer' />
-                </div>
-
-                <div className="links">
-                    <div className="top-links">
-
-                        <NavLink href='/' onClick={closeMenu}>
-                            <LayoutDashboard size={32} />
-                            <span className="text">Dashboard</span>
-                        </NavLink>
-
-                        <NavLink href='/inventory' onClick={closeMenu}>
-                            <MonitorSmartphone size={32} />
-                            <span className="text">Inventory</span>
-                        </NavLink>
-
-                        <NavLink href='/assignments' onClick={closeMenu}>
-                            <Users size={32} />
-                            <span className="text">Assignments</span>
-                        </NavLink>
-
-                        <NavLink href='/logs' onClick={closeMenu}>
-                            <ClipboardList size={32} />
-                            <span className="text">Logs</span>
-                        </NavLink>
-                    </div>
-
-
-                    <div className="bottom-links">
-                        <NavLink href='/help' onClick={closeMenu}>
-                            <HelpCircle size={32} />
-                            <span className="text">Help</span>
-                        </NavLink>
-
-                        {/* <button className='navlink' id="logout" onClick={closeMenu}>
-                            <LogOut size={32} />
-                            <span className="text">Logout</span>
-                        </button> */}
-                    </div>
-                </div>
-
-
-            </div>
+          <X size={32} onClick={closeMenu} className="cursor-pointer" />
         </div>
-    )
-}
 
-export default MobileMenu
+        <div className="links">
+          <div className="top-links">
+            <NavLink href="/" onClick={closeMenu}>
+              <LayoutDashboard size={32} />
+              <span className="text">Dashboard</span>
+            </NavLink>
+
+            <NavLink href="/inventory" onClick={closeMenu}>
+              <MonitorSmartphone size={32} />
+              <span className="text">Inventory</span>
+            </NavLink>
+          </div>
+
+          <div className="bottom-links">
+            <NavLink href="/help" onClick={closeMenu}>
+              <HelpCircle size={32} />
+              <span className="text">Help</span>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
